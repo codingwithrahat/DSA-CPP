3342b6ed7c07bf90e065c6d3679b5fbc27829418
Mohammad Rahat Hossan <131850941+codingwithrahat@users.noreply.github.com>
2025-04-21 15:00:17 +0600

    Update insertion_sort.cpp

--- Sorting Algorithms/insertion_sort.cpp
+++ Sorting Algorithms/insertion_sort.cpp
@@ -1,1 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+void insertionSort(int a[], int n){
+    for(int i = 1; i<n; i++){
+        int key = a[i];
+        int j = i - 1;
+        while(a[j] > key && j >= 0){
+            a[j + 1] = a[j];
+            j--;
+        }
+        a[j + 1] = key;
+    }
+}
+
+int main(){
+    int n; cin>>n;
+
+    int a[n];
+
+    for(int i = 0; i<n; i++) cin>>a[i];
+
+    insertionSort(a, n);
+    
+    for(int i = 0; i<n; i++) cout<<a[i]<<" ";
+    cout<<endl;
+}
