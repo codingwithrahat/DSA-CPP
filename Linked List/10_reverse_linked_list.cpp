6ce0442a0275fe2a86f1bfb8bc4055bfb0d3b714
codingwithrahat <codingwithrahat@gmail.com>
2026-02-11 00:53:13 +0600

    d

--- Linked List/10_reverse_linked_list.cpp
+++ Linked List/10_reverse_linked_list.cpp
@@ -1,27 +1,27 @@
 //https://leetcode.com/problems/reverse-linked-list/description/
 
 
-class Solution {
-public:
-    ListNode* reverseList(ListNode* head) {
-        ListNode* cur = head;
-        ListNode* prev = head;
-        ListNode* next = head;
+// class Solution {
+// public:
+//     ListNode* reverseList(ListNode* head) {
+//         ListNode* cur = head;
+//         ListNode* prev = head;
+//         ListNode* next = head;
 
-        prev = NULL;
+//         prev = NULL;
 
-        while(cur != NULL){
+//         while(cur != NULL){
             
-            next = cur->next;
-            cur->next = prev;
+//             next = cur->next;
+//             cur->next = prev;
             
-            prev = cur;
-            cur = next;
+//             prev = cur;
+//             cur = next;
             
 
-        }
+//         }
 
-        return prev;
+//         return prev;
 
-    }
-};
+//     }
+// };
